9dcb057645f44f708c21aba91059c3e141878258
MartinF-20 <73145443+MartinF-20@users.noreply.github.com>
2022-03-24 21:18:49 -0600

    Ejercicio_3
    Ejercicio_3

---
+++ Ejercicio_3/Ejercicio_3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Ejercicio_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.aceptar = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 133);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(707, 265);
+            this.textBox2.TabIndex = 1;
+            // 
+            // aceptar
+            // 
+            this.aceptar.Location = new System.Drawing.Point(619, 62);
+            this.aceptar.Name = "aceptar";
+            this.aceptar.Size = new System.Drawing.Size(75, 23);
+            this.aceptar.TabIndex = 2;
+            this.aceptar.Text = "Aceptar";
+            this.aceptar.UseVisualStyleBackColor = true;
+            this.aceptar.Click += new System.EventHandler(this.aceptar_Click);
+            // 
+            // igual
+            // 
+            this.igual.Location = new System.Drawing.Point(700, 62);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(75, 23);
+            this.igual.TabIndex = 3;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = true;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.aceptar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button aceptar;
+        private System.Windows.Forms.Button igual;
+    }
+}
+
---
+++ Ejercicio_3/Ejercicio_3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox1 = (TextBox)sender;
+
+            if (textBox1.Text.Length <= 0) return;
+            bool isRight = Double.TryParse(textBox1.Text, out _);
+
+            if (!isRight)
+            {
+                MessageBox.Show("Dato no válido");
+                String res = "";
+                for (int i = 0; i < textBox1.TextLength; i++)
+                {
+                    switch (textBox1.Text[i])
+                    {
+                        case '.':
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            res += textBox1.Text[i];
+                            break;
+                    }
+                    
+
+                    if(textBox1.Text[i] >= '0' && textBox1.Text[i] <= '9')
+                    {
+                        res += textBox1.Text[i];
+                    }
+                }
+            }
+
+            //char last = textBox1.Text[textBox1.Text.Length - 1];
+            /*if (!(last >= '0' && last <= '9'))
+            {
+                MessageBox.Show("Datos no validos");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }*/
+        }
+
+        private void aceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            //DialogResult.AppendText(total.ToString());
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) && !(e.KeyChar=='.'))
+            {
+                e.Handled = false;
+            }
+        }
+    }
+}
