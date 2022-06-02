d553bcac127e18da49a975c1dbdd02de55c2beba
MartinF-20 <73145443+MartinF-20@users.noreply.github.com>
2022-06-01 21:31:27 -0500

    Add files via upload

---
+++ Practica5/Practica1/Practica1/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text == "Martin" && txtContraseña.Text == "12345")
+            {
+                Navigation.PushAsync(new Home());
+            }
+            else
+            {
+                DisplayAlert("Ops...","Nombre de usuario o contraseña incorrecto","Ok");
+            }
+        }
+    }
+}
