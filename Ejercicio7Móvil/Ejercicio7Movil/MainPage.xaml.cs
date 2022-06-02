6aa7cdd3f2d6e991c85e8615f40bd913262e57fd
MartinF-20 <73145443+MartinF-20@users.noreply.github.com>
2022-06-01 21:53:15 -0500

    Add files via upload

---
+++ Ejercicio7Móvil/Ejercicio7Movil/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+
+namespace Ejercicio7Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        string _filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notas.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(File.Exists(_filename)) 
+            {
+                File.Delete(_filename);
+            }
+            entrada.Text = string.Empty;
+        }
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            File.WriteAllText(_filename, entrada.Text);
+        }
+    }
+}
---
+++ Ejercicio7Móvil/Ejercicio7Movil/MeuPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio7Movil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MeuPage : TabbedPage
+    {
+        public MeuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
