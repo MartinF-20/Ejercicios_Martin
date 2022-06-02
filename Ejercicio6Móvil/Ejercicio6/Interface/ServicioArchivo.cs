0d92e17657e3399b3cafe53f5e3718e0da3dce8f
MartinF-20 <73145443+MartinF-20@users.noreply.github.com>
2022-06-01 21:52:33 -0500

    Add files via upload

---
+++ Ejercicio6Móvil/Ejercicio6/Interface/ServicioArchivo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio6.Interface
+{
+    public interface ServicioArchivo
+    {
+        bool CrearArchivo(string contenido, string nombre);
+        bool BorrarArchivo(string nombre);
+    }
+
+}
---
+++ Ejercicio6Móvil/Ejercicio6/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Ejercicio6.Interface;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_guardar_Clicked (object sender, EventArgs e)
+        {
+
+            if(String.IsNullOrWhiteSpace(txt_entrada.Text))
+            {
+                await DisplayAlert("Alerta", "No puedes guardar notas vacias", "OK");
+            }
+            else
+            {
+                var contenido = txt_entrada.Text;
+                var result = await DisplayPromptAsync("Nombre", "Como se va a llamar tu nota?");
+
+                if (String.IsNullOrWhiteSpace(result))
+                {
+                    await DisplayAlert("Alerta", "No puedes guardar notas sin nombre", "OK");
+                }
+                else
+                {
+                    if (DependencyService.Get<ServicioArchivo>().CrearArchivo(contenido, result) == true)
+                    {
+                        await DisplayAlert("Alerta", "Archivo guardado correctamente", "OK");
+                        txt_entrada.Text = "";
+                    }
+                    else
+                    {
+                        await DisplayAlert("Alerta", "Ya existe una nota con ese nombre", "OK");
+                    }
+                }        
+            }
+        }
+
+        private async void btn_borrar_Clicked(object sender, EventArgs e)
+        {
+
+            var nombreBorrar = await DisplayPromptAsync("Nombre", "Como se llama la nota que quieres borrar?");
+
+            if(DependencyService.Get<ServicioArchivo>().BorrarArchivo(nombreBorrar) == true)
+            {
+                await DisplayAlert("Alerta", "El archivo " + nombreBorrar + ".txt se borró correctamente", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "No existe un archivo con ese nombre", "OK");
+            }
+
+        }
+    }
+}
